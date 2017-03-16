1816a987695cc1f9031eddcc86c38c92d73fc55d
mgarewal <mandy_garewal@rapid7.com>
2017-03-16 15:10:14 -0400

    Added unit test for _load_parameters_json()

--- src/TestJsonConfig.py
+++ src/TestJsonConfig.py
@@ -1,12 +1,23 @@
 import unittest
 import json
 import metrics
+import config
 
 
 class TestJsonConfig(unittest.TestCase):
+    user_key = ''
+    api_key = ''
+    agent_key = ''
+    formatters = ''
+    formatter = ''
+    hostname = ''
+    pull_server_side_config = ''
+    entry_identifier = ''
+    filters = ''
+
     json_file = '''{
       "config": {
-        "name": "mgarewal",
+        "hostname": "mgarewal",
         "endpoint": "data.logentries.com",
         "user-key": "4a22b0ba-593a-41e9-9271-bf4475b09f38",
         "agent-key": "754302eb-778d-4dc6-8043-c8465826c90b",
@@ -34,23 +45,24 @@ class TestJsonConfig(unittest.TestCase):
       }
     }'''
 
+    # Read in json config file
+    d_conf = json.loads(json_file)
+    d_configFile = d_conf['config']
+
     #    test the metrics.load_json() method correctly pulls the right parameters and associated values.
     def test_metrics_load_json(self):
         self.metrics = metrics.MetricsConfig()
-        expected_dict = {'net': 'sum eth0', 'space' : '/', 'disk': 'sum sda4 sda5', 'interval': '60s', 'swap': 'system', 'vcpu': 'core', 'cpu': 'system', 'mem': 'system', 'token': None}
+        expected_dict = {'net': 'sum eth0', 'space' : '/', 'disk': 'sum sda4 sda5', 'interval': '60s', 'swap': 'system',
+                         'vcpu': 'core', 'cpu': 'system', 'mem': 'system', 'token': None}
 
-        # Read in json config file
-        d_conf = json.loads(self.json_file)
-        d_configFile = d_conf['config']
-
-        result_dict = self.metrics.load_json(d_configFile)
+        result_dict = self.metrics.load_json(self.d_configFile)
         print(result_dict)
 
         self.assertDictEqual(result_dict, expected_dict, msg=None)
 
     # test the _load_configured_logs_json() method correctly pulls the right parameters and associated values.
     def test_load_configured_logs_json(self):
-        expected_dict =           {
+        expected_dict = {
             "name": "GreenLog",
             "token": "09da4e87-882e-41f1-bf50-5f45273ed180",
             "path": "/var/log/GreenLog",
@@ -59,10 +71,26 @@ class TestJsonConfig(unittest.TestCase):
             "destination" : None
           }
 
-        d_conf = json.loads(self.json_file)
-        d_configFile = d_conf['config']
+        result_dict = self.config._load_configured_logs_json(self.d_configFile)
 
-        result_dict = self._load_configured_logs_json(d_configFile)
+        self.assertDictEqual(result_dict, expected_dict, msg=None)
+
+    # test
+    def test_load_parameters_json(self):
+        expected_dict = {'user-key': "4a22b0ba-593a-41e9-9271-bf4475b09f38",
+                          'agent_key_param': "754302eb-778d-4dc6-8043-c8465826c90b",
+                          'api_key_param': "0e099781-bd98-46a5-8c6a-1129a043c058", 'formatter': None,
+                          'formatters': None, 'entry-identifier': None,  'hostname': "mgarewal",
+                          'pull-server-side-config': "true"}
+
+        result_dict = {}
+
+        config._load_parameters_json(self.d_configFile)
+
+        result_dict.update({'user-key': self.user_key, 'agent-key' : self.agent_key,
+                    'api-key': self.api_key, 'filters': self.filters, 'formatters': self.formatters,
+                    'formatter': self.formatter, 'entry_identifier': self.entry_identifier,
+                    'hostname': self.hostname, 'pull-server-side-config': self.pull_server_side_config})
 
         self.assertDictEqual(result_dict, expected_dict, msg=None)
 
